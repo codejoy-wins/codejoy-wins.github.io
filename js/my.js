d29a0f5263d07f08470501dc29d6cdf69b2757af
Max <mjann@ucsc.edu>
2019-01-16 03:44:58 -0800

    best commit

---
+++ js/my.js
@@ -0,0 +1,17 @@
+console.log("codejoy is alive");
+function dragon(){
+    var target = document.getElementById("fire");
+    // do something cool to target
+    // play audio
+    var audio = new Audio('Dragon.wav');
+    audio.play();
+}
+function drogon(){
+    var target = document.getElementById("fire");
+    // do something cool to target
+    // play audio
+    // set interval
+    console.log("805 862 7047")
+    var audio = new Audio('Dragon.wav');
+    audio.play();
+}
